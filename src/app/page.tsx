1a8a5be5e21a1cd1c9f0c1f8d707b5e4621422f2
Farid Amimri <farid.amimri@gmail.com>
2024-02-17 23:40:07 +0100

    Design Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 /** @format */
 
+import Hero from '@/_components/Hero'
 import Image from 'next/image'
 
 export default function Home() {
-  return <div>hey</div>
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
 }
