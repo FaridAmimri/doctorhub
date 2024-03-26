a0ce11056294fe6a63b70c1e4d5c0ae5be665969
Farid Amimri <farid.amimri@gmail.com>
2024-03-26 10:43:12 +0100

    Handele profil with popover using shadc/ui

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -4,7 +4,12 @@
 import { Stethoscope } from 'lucide-react'
 import Link from 'next/link'
 import { useSession, signOut } from 'next-auth/react'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover'
+import Image from 'next/image'
 
 const Header = () => {
   const { data: session } = useSession()
@@ -52,15 +57,47 @@ const Header = () => {
           Get Started
         </Link>
       ) : (
-        <div className='flex gap-2 items-center'>
-          <Avatar>
-            <AvatarImage src={session?.user?.image!} alt='profil' />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <span className='cursor-pointer' onClick={() => signOut()}>
-            Logout
-          </span>
-        </div>
+        <Popover>
+          <PopoverTrigger>
+            {session.user.image ? (
+              <Image
+                src={session.user.image}
+                alt='profile-image'
+                width={40}
+                height={40}
+                className='rounded-full'
+              />
+            ) : (
+              <Image
+                src={
+                  'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'
+                }
+                alt='profile-image'
+                width={40}
+                height={40}
+                className='rounded-full'
+              />
+            )}
+          </PopoverTrigger>
+          <PopoverContent className='w-44'>
+            <ul className='flex  flex-col gap-2'>
+              <Link
+                href={'/my-booking'}
+                className='cursor-pointer
+             hover:bg-slate-100 p-2 rounded-md'
+              >
+                My Booking
+              </Link>
+              <li
+                className='cursor-pointer
+             hover:bg-slate-100 p-2 rounded-md'
+                onClick={() => signOut()}
+              >
+                Logout{' '}
+              </li>
+            </ul>
+          </PopoverContent>
+        </Popover>
       )}
     </header>
   )
